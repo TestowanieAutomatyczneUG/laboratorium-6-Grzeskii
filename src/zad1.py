d51ce5847ca4845705bb0c712592098ad36e1efe
grzeski <grzegorz.rzeski@autonomik.pl>
2020-11-10 12:19:22 +0100

    preparing for tests

--- src/zad1.py
+++ src/zad1.py
@@ -1,5 +1,14 @@
+class FizzBuzz:
 
-
-
-
+    def game(self, num):
+        if type(num) != int:
+            raise Exception("Not a valid number")
+        if int(num) % 15 == 0:
+            return "\"FizzBuzz\""
+        elif int(num) % 3 == 0:
+            return "\"Fizz\""
+        elif int(num) % 5 == 0:
+            return "\"Buzz\""
+        else:
+            raise Exception("Indivisible number")
 
